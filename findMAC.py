6e2be39d903b2d04119dad8fb8cbf1374ba36fbc
admiralspark <admiralspark@gmail.com>
2016-12-05 19:34:20 -0900

    Added support for outputting config of port if a CDP hit is found

--- findMAC.py
+++ findMAC.py
@@ -36,6 +36,7 @@ def findmac(username, password, secret):
             }
 
             net_connect = ConnectHandler(**switch)
+            net_connect.enable()
             macreturn = net_connect.send_command(command_string_mac)
             cdpreturn = net_connect.send_command(command_string_cdp)
 
@@ -51,9 +52,8 @@ def findmac(username, password, secret):
                 port = cdpList.split(' ')[3]
                 command_string_port = 'show run int ' + str(port)
                 portreturn = net_connect.send_command(command_string_port)
-                print "\nPort Configuration for matched CDP or MAC: "
-                # BROKENNNNN
-                #print portreturn
+                print "\nPort Configuration for matched CDP: "
+                print portreturn
             print "\n>>>>>>>>> End <<<<<<<<<"
 
             net_connect.disconnect()
--- localusers.py
+++ localusers.py
@@ -1,19 +1,20 @@
 '''
-This script logs in to switches, checks the existing local usernames against
-the allowed list, and remediates them if inconsistencies are found.
+This script will standardize on specific usernames and passwords to
+make sure that everything, everywhere, has the same backup accounts.
 '''
 
-#Global imports
+# Global
 from netmiko import ConnectHandler
-from datetime import datetime
 import csv, os.path
-#Local imports
+from datetime import datetime
+
+# Local
 import credentials
 
 # Begin timing Script
 start_time = datetime.now()
 
-def lu (username, password, secret):
+def nc(username, password, secret, customer, command_string):
     with open(customer, mode='r') as csvfile:
         reader = csv.DictReader(csvfile)
 
@@ -31,22 +32,25 @@ def lu (username, password, secret):
             }
 
             net_connect = ConnectHandler(**switch)
-            # command_string_mac = 'show mac add | inc ' + str(macAddr)
-            # macreturn = net_connect.send_command(command_string_mac)
-
+            net_connect.enable()
+            connect_return = net_connect.send_command_expect(command_string)
             print "\n\n>>>>>>>>> Device {0} <<<<<<<<<".format(row['SysName'])
-            
+            print connect_return
             print "\n>>>>>>>>> End <<<<<<<<<"
-
             net_connect.disconnect()
 
-# Grab the Customer name to search
-customer = raw_input('Customer name: ') + ".csv"
-# Flesh out these variables using the credentials.cred_csv module
-username, password, secret = credentials.cred_csv()
-# Run the primary function in this program
-lu(username, password, secret)
 
+# Proper way
+# customer = raw_input('Customer name: ') + ".csv"
+customer = "HOME.csv"
+# Load credentials
+username, password, secret = credentials.cred_csv()
+# Temp input until this is working
+command_string = "sh run | inc username"
+# Now call the function
+nc(username, password, secret, customer, command_string)
+
+#End the time counter
 end_time = datetime.now()
 # How long did it run?
 total_time = end_time - start_time
