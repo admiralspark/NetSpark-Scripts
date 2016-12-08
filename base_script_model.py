105a816f72ef5ed828ee7b2151e94f2f6f24e3f7
Kristopher Klunder <admiralspark@gmail.com>
2016-12-07 15:42:16 -0900

    Updated the base script in prep for more work

--- base_script_model.py
+++ base_script_model.py
@@ -1,5 +1,11 @@
-import netmiko
+#Global imports
+from netmiko import ConnectHandler
+from datetime import datetime
 import csv, os.path
+#Local imports
+import credentials
+
+start_time = datetime.now()
 
 def nc(username, password, secret, customer, command_string):
     with open(customer, mode='r') as csvfile:
@@ -19,5 +25,24 @@ def nc(username, password, secret, customer, command_string):
             }
 
             net_connect = ConnectHandler(**switch)
+            # Insert enables et al here
+            # net_connect.enable()
+            command_string = "write mem"
             connect_return = net_connect.send_command(command_string)
+            print "\n\n>>>>>>>>> Device {0} <<<<<<<<<".format(row['SysName'])
+            print connect_return
+            print "\n>>>>>>>>> End <<<<<<<<<"
+            # Disconnect from this session
             net_connect.disconnect()
+
+# Grab the Customer name to search
+customer = raw_input('Customer name: ') + ".csv"
+# Flesh out these variables using the credentials.cred_csv module
+username, password, secret = credentials.cred_csv()
+# Run the primary function in this program
+nc(username, password, secret)
+
+end_time = datetime.now()
+# How long did it run?
+total_time = end_time - start_time
+print "\nTotal time for script: \n" + str(total_time)
